c7cd096b4391e9d0333af47e8beaf0a532eda2b9
mbliefer <mblief@gmail.com>
2023-05-21 20:23:18 -0500

    script

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const password = document.querySelector("#password");
+const passwordInput = document.querySelector("#password");
 const confirmPassword = document.querySelector("#password-confirm");
+const eightChars = document.querySelector(".eightChars");
+const uCase = document.querySelector(".uCase");
+const lCase = document.querySelector(".lCase");
+const num = document.querySelector(".num");
 
+let password = "";
+let hasNumber = /\d/;
+
+function checkPassword() {
+    password = passwordInput.value;
+    console.log(password);
+    console.log(password.length);
+    if (password.length >= 8) {
+        eightChars.style.color = "lightgreen";
+    }
+    if (password.toLowerCase() !== password) {
+        uCase.style.color = "lightgreen";
+    }
+    if (password.toUpperCase() !== password) {
+        lCase.style.color = "lightgreen";
+    }
+    if (hasNumber.test(password)) {
+        num.style.color = "lightgreen";
+    }
+}
+
+
+passwordInput.addEventListener('keyup', checkPassword);
